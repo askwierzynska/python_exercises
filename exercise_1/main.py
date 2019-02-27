44aefdd368117095267f0422799a0343680f28c2
askwierzynska <a.skwierzynska@protonmail.com>
2019-02-27 21:22:51 +0100

    added ex_1

---
+++ exercise_1/main.py
@@ -0,0 +1,11 @@
+print('/////////////')
+print('hello app')
+print('/////////////')
+print(' ')
+
+imie = input('jak masz na imie? ')
+greeting = ('milego dnia ' + imie + '!')
+
+print()
+print(greeting)
+
